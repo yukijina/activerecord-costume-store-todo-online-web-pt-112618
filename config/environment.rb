0711f32a66b12e5346a8ec852f64d43d5b6d41c9
Yukiji Nakatsuka <lunarbase@outlook.com>
2019-02-19 02:12:52 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -6,6 +6,7 @@ require 'active_record'
 require 'yaml/store'
 require 'ostruct'
 require 'date'
+require 'pry'
 
 DBNAME = "halloween"
 
--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,22 @@
 # Create your costume_stores migration here
+
+class CreateCostumeStores < ActiveRecord::Migration[4.2] 
+  def up 
+  end 
+  
+  def down 
+  end 
+  
+  def change
+    create_table :costume_stores do |t| 
+      t.string :name
+      t.text :location
+      t.float :costume_inventory
+      t.integer :num_of_employees
+      t.boolean :still_in_business
+      t.datetime :opening_time
+      t.datetime :closing_time
+    end 
+  end 
+  
+end 
